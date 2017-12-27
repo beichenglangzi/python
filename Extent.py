c02637ffceffeb47deca072569d74b8f9d47ad37
cl <861472308@qq.com>
2017-12-27 18:03:08 +0800

    修改了一些代码

---
+++ Extent.py
@@ -0,0 +1,24 @@
+class Point:
+   def __init__( self, x=0, y=0):
+      self.x = x
+      self.y = y
+   def __del__(self):
+      class_name = self.__class__.__name__
+      print (class_name, "销毁")
+
+
+class Employee:
+    '所有员工的基类'
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print ("Total Employee %d" % Employee.empCount)
+
+    def displayEmployee(self):
+        print ("Name : ", self.name, ", Salary: ", self.salary)
+
